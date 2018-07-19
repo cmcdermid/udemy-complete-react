9f3f7633c6da4576cfa282bdf48ede508ecc010b
cmcdermid <chris.mcdermid@gmail.com>
2018-07-19 14:50:15 +1000

    [S5L46][Chris] Local browser storage

--- src/app.js
+++ src/app.js
@@ -6,14 +6,26 @@ class IndecisionApp extends React.Component {
         this.handlePick = this.handlePick.bind(this);
         this.handleAddOption = this.handleAddOption.bind(this);
         this.state = {
-            options: props.options
+            options: []
         };
     }
     componentDidMount() {
-        console.log('componentDidMount!');
+        try {
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+
+            if (options) {
+                this.setState(() => ({ options }))
+            }
+        } catch (e) {
+            // Do nothing at all
+        }
     }
-    componentDidUpdate() {
-        console.log('componentDidUpdate!');
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.options.length !== this.state.options.length) {
+            const json = JSON.stringify(this.state.options);
+            localStorage.setItem('options', json);
+        }
     }
     componentWillUnmount() {
         console.log('componentWillUnmount!');
@@ -26,7 +38,7 @@ class IndecisionApp extends React.Component {
         this.setState((prevState) => ({
             options: prevState.options.filter((option) => optionToRemove !== option)
         }));
-        console.log('hdo', option);
+        console.log('hdo', optionToRemove);
     }
     handlePick() {
         const randomNum = Math.floor(Math.random() * this.state.options.length);
@@ -65,10 +77,6 @@ class IndecisionApp extends React.Component {
     }
 }
 
-IndecisionApp.defaultProps = {
-    options: []
-};
-
 const Header = (props) => {
     return (
         <div>
@@ -99,6 +107,7 @@ const Options = (props) => {
     return (
         <div>
             <button onClick={props.handleDeleteOptions}>Remove All</button>
+            {props.options.length === 0 && <p>Please add an option to get started!</p>}
             {
                 props.options.map((option) => (
                     <Option
@@ -141,6 +150,10 @@ class AddOption extends React.Component {
         const option = e.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option);
         this.setState(() => ({ error }));
+
+        if (!error) {
+            e.target.elements.option.value = '';
+        }
     }
     render() {
         return (
