c4aaaf41fb34035d4497503dacdc564d2da80e5f
cmcdermid <chris.mcdermid@gmail.com>
2018-07-23 17:05:29 +1000

    [S7L62][Chris] Completed Third Party Component section using Modals

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,10 +3,12 @@ import AddOption from './AddOption';
 import Header from './Header';
 import Action from './Action';
 import Options from './Options';
+import OptionModal from './OptionModal';
 
 export default class IndecisionApp extends React.Component {
     state = {
-        options: []
+        options: [],
+        selectedOption: undefined
     };
     handleDeleteOptions = () => {
         this.setState(() => ({ options: [] }));
@@ -20,7 +22,7 @@ export default class IndecisionApp extends React.Component {
     handlePick = () => {
         const randomNum = Math.floor(Math.random() * this.state.options.length);
         const option = this.state.options[randomNum];
-        alert(option);
+        this.setState(() => ({ selectedOption: option}));
     };
     handleAddOption = (option) => {
         if (!option) {
@@ -30,6 +32,9 @@ export default class IndecisionApp extends React.Component {
         }
 
         this.setState((prevState) => ({ options: prevState.options.concat(option) }));
+    };
+    handleClearSelectedOption = () => {
+        this.setState(() => ({ selectedOption: undefined }))
     };
     componentDidMount() {
         try {
@@ -70,6 +75,10 @@ export default class IndecisionApp extends React.Component {
                 />
                 <AddOption
                     handleAddOption={this.handleAddOption}
+                />
+                <OptionModal
+                    selectedOption={this.state.selectedOption}
+                    handleClearSelectedOption={this.handleClearSelectedOption}
                 />
             </div>
         );
