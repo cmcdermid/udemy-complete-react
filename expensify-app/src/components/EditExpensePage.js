36db6fe384f57881437cdb82b08890008adddfac
cmcdermid <chris.mcdermid@gmail.com>
2018-07-24 23:30:50 +1000

    [S9L81][Chris] React router with navigation and parameter links

---
+++ expensify-app/src/components/EditExpensePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EditExpensePage = (props) => {
+    console.log(props);
+    return (
+        <div>
+            Editting the expense with id of {props.match.params.id}
+        </div>
+    )
+};
+
+export default EditExpensePage;
